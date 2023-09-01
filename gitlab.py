cff00c67007196be04bcb3bb3fdf8aa0e6f89ac9
Matheus Theodoro <matheustheodoro3112@gmail.com>
2023-09-01 14:49:41 -0300

    script file

---
+++ gitlab.py
@@ -0,0 +1,8 @@
+def multiplier(a,b):
+    result = a * b
+    return result
+
+
+question1 = int(input('type a number: '))
+question2 = int(input('type another number: '))
+print(f'the product of those numbers is {multiplier(question1,question2)}')
